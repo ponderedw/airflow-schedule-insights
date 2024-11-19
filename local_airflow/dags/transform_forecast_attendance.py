e72c466d90018cdf142226fa4a54e4cde536a0c7
Egor Tarasenko <egor@ponder.co>
2024-11-19 11:06:35 +0200

    FEAT: Rename secondary_dag_7 to transform_forecast_attendance

--- local_airflow/dags/transform_forecast_attendance.py
+++ local_airflow/dags/transform_forecast_attendance.py
@@ -4,10 +4,10 @@ from airflow.datasets import Dataset
 import time
 
 
-@dag(dag_id='secondary_dag_7', max_active_runs=1, start_date=datetime(2023, 1, 1),
+@dag(dag_id='transform_forecast_attendance', max_active_runs=1, start_date=datetime(2023, 1, 1),
      is_paused_upon_creation=False, catchup=False, schedule_interval=(Dataset('transform_sales_aggregator') & Dataset('transform_customer_sentiment')))
 def dag_test():
-    @task(outlets=[Dataset('secondary_dag_7')])
+    @task(outlets=[Dataset('transform_forecast_attendance')])
     def end_task():
         time.sleep(25)
         a = 1 / 0

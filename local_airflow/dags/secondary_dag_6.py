f8fbcc3933793a08fdd3fd107a0b33c905d38c7f
Egor Tarasenko <egor@ponder.co>
2024-11-19 11:03:16 +0200

    FEAT: Rename secondary_dag_4 to transform_movie_performance

--- local_airflow/dags/secondary_dag_6.py
+++ local_airflow/dags/secondary_dag_6.py
@@ -5,7 +5,7 @@ import time
 
 
 @dag(dag_id='secondary_dag_6', max_active_runs=1, start_date=datetime(2023, 1, 1),
-     is_paused_upon_creation=False, catchup=False, schedule_interval=(Dataset('transform_inventory_optimization') & Dataset('secondary_dag_4')))
+     is_paused_upon_creation=False, catchup=False, schedule_interval=(Dataset('transform_inventory_optimization') & Dataset('transform_movie_performance')))
 def dag_test():
     @task(outlets=[Dataset('secondary_dag_6')])
     def end_task():
--- local_airflow/dags/transform_movie_performance.py
+++ local_airflow/dags/transform_movie_performance.py
@@ -4,10 +4,10 @@ from airflow.datasets import Dataset
 import time
 
 
-@dag(dag_id='secondary_dag_4', max_active_runs=1, start_date=datetime(2023, 1, 1),
+@dag(dag_id='transform_movie_performance', max_active_runs=1, start_date=datetime(2023, 1, 1),
      is_paused_upon_creation=False, catchup=False, schedule_interval=(Dataset('transform_inventory_optimization')))
 def dag_test():
-    @task(outlets=[Dataset('secondary_dag_4')])
+    @task(outlets=[Dataset('transform_movie_performance')])
     def end_task():
         time.sleep(25)
 
